e0c17a33287aea2594ba29905efd5427f4b14da2
MarrouDamien <damien.marrou@gmail.com>
2022-01-13 09:10:55 +0000

    save before API REST

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -1,31 +1,41 @@
-// src/app/app.component.ts
+// src/app/app.component.ts 
 
-// Le décorateur Component permet de donner à Angular des informations
-// supplémentaires sur une classe afin d'expliquer que :
-// 1. C'est un composant ;
-// 2. Il devra afficher un template HTML donné ;
-// 3. Il aura des styles scopés ;
-// Et beaucoup d'autres choses encore
 import { Component } from '@angular/core';
+import { Tasks } from './types/task';
 
 @Component({
-  // Le sélecteur du composant permet de dire à Angular
-  // "Quand tu vois une balise <app-root>, remplaces la
-  // par le rendu de ce composant
   selector: 'app-root',
-  // Le template représente le HTML qui sera rendu par ce composant
-  // Il peut contenir des balises HTML classiques comme des
-  // appels à d'autres composants Angular
   template: `
     <div id="app">
         <h1>La Todo App</h1>
 
-        <main></main>
+        <main>
+          <app-todo-list 
+            [tasks]="tasks" 
+          ></app-todo-list>
+          <app-task-form 
+            (onNewTask)="addTask($event)"
+          ></app-task-form>
+        </main>
     </div>
   `,
-  // Les styles nous permettent de créer des styles CSS *scopés*
-  // C'est à dire que les règles définies ici ne s'appliqueront que sur
-  // le template de ce composant, et pas en dehors
   styles: []
 })
-export class AppComponent { }
+export class AppComponent {
+  tasks: Tasks = [
+    { id: 1, text: "Aller faire des courses", done: false },
+    { id: 2, text: "Faire à manger", done: true },
+  ];
+
+  // La méthode addTask recevra une string
+  addTask(text: string) {
+    // Elle s'en servira pour créer une nouvelle tâche dans 
+    // le tableau des tâches, et Angular mettra à jour 
+    // l'affichage afin d'en tenir compte !
+    this.tasks.push({
+      id: Date.now(),
+      text: text,
+      done: false
+    });
+  }
+}
